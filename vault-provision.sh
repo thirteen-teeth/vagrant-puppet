113b31801b4d6e7982a6fa084c4435860eec254b
thirteen-teeth <emorris2@outlook.com>
2021-08-24 15:16:35 -0400

    changes

--- vault-provision.sh
+++ vault-provision.sh
@@ -30,9 +30,44 @@ export VAULT_ADDR=http://localhost:8200
 export PATH=$PATH:/usr/local/bin
 vault operator init -key-shares=1 -key-threshold=1 > /opt/vault_data/full_output.txt
 
-unseal_key=$(cat /opt/vault_data/full_output.txt | head -n 1 | awk {'print $(NF)'})
+unseal_key=$(grep 'Unseal Key 1' /opt/vault_data/full_output.txt | awk '{print $NF}')
 echo $unseal_key > /opt/vault_data/unseal_key
-initial_token=$(cat /opt/vault_data/full_output.txt | sed -n '3 p' | awk {'print $(NF)'})
+initial_token=$(grep 'Initial Root Token' /opt/vault_data/full_output.txt | awk '{print $NF}')
 echo $initial_token > /opt/vault_data/initial_token
+export VAULT_TOKEN=$initial_token
+
+vault operator unseal $unseal_key
+vault secrets enable consul
+vault secrets enable pki
+vault secrets tune -max-lease-ttl=87600h pki
+vault write -field=certificate pki/root/generate/internal \
+      common_name="puppetdomain" \
+      ttl=87600h > CA_cert.crt
+vault write pki/config/urls \
+      issuing_certificates="$VAULT_ADDR/v1/pki/ca" \
+      crl_distribution_points="$VAULT_ADDR/v1/pki/crl"
+
+vault secrets enable -path=pki_int pki
+vault secrets tune -max-lease-ttl=43800h pki_int
+
+vault write -format=json pki_int/intermediate/generate/internal \
+      common_name="puppetdomain Intermediate Authority" \
+      | jq -r '.data.csr' > pki_intermediate.csr
+
+vault write -format=json pki/root/sign-intermediate csr=@pki_intermediate.csr \
+      format=pem_bundle ttl="43800h" \
+      | jq -r '.data.certificate' > intermediate.cert.pem
+
+vault write pki_int/intermediate/set-signed certificate=@intermediate.cert.pem
+
+vault write pki_int/roles/puppetdomain \
+      allowed_domains="puppetdomain" \
+      allow_subdomains=true \
+      max_ttl="720h"
 
 exit 0
+
+#export VAULT_ADDR=http://vault-01.puppetdomain:8200
+#export PATH=$PATH:/usr/local/bin
+#initial_token=$(cat /opt/vault_data/full_output.txt | sed -n '3 p' | awk {'print $(NF)'})
+#export VAULT_TOKEN=$initial_token
